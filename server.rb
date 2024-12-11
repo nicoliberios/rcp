3a7cc13914eaa473e400e084637102ca9eb0b0b5
nicoliberios <ingedesistemas24@gmail.com>
2024-12-11 16:32:53 -0500

    This is a new project

--- server.rb
+++ server.rb
@@ -1,17 +1,14 @@
-# server.rb
+
 require 'drb/drb'
 
-# Crear un objeto que se va a compartir
 class HelloWorld
   def say_hello
     "Hola Mundo desde RPC en Ruby!"
   end
 end
 
-# Dirección donde el servidor escuchará las solicitudes
 URI = "druby://localhost:8787"
 
-# Crear y registrar el objeto para que sea accesible remotamente
 DRb.start_service(URI, HelloWorld.new)
 puts "Servidor RPC en Ruby en funcionamiento..."
 DRb.thread.join
